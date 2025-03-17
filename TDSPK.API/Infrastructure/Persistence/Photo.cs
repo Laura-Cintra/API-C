26732fae9068bd7ad597b27ec4d08f34fb8b16fa
Laura Cintra <79454879+Laura-Cintra@users.noreply.github.com>
2025-03-17 12:10:13 -0300

    feat: reposity generic

---
+++ TDSPK.API/Infrastructure/Persistence/Photo.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TDSPK.API.Domain;
+
+namespace TDSPK.API.Infrastructure.Persistence
+{
+    public class Photo
+    {
+        public Guid Id { get; private set; }
+
+        //public User User { get; private set; }
+
+        [Url]
+        public string Url { get; private set; }
+
+        public DateTime Date { get; private set; }
+
+        public Photo(string url)
+        {
+            if (url == string.Empty) throw new Exception("Url vazia");
+
+            Id = Guid.NewGuid();
+            Date = DateTime.Now;
+            Url = url;
+
+            //User = user;
+        }      
+    }
+}
---
+++ TDSPK.API/Infrastructure/Persistence/User.cs
@@ -0,0 +1,16 @@
+﻿using TDSPK.API.Domain;
+using TDSPK.API.Domain.Enums;
+
+namespace TDSPK.API.Infrastructure.Persistence
+{
+    public class User : Audit
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }      
+
+        public User()
+        {
+            Status = StatusType.Active;
+        }
+    }
+}
