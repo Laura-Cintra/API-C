968ddae03ef542cc92faf7f5a8c5c961256b220f
Laura Cintra <lauraocintra@gmail.com>
2025-04-07 11:53:19 -0300

    feat: relacionamento photo e user

--- TDSPK.API/Infrastructure/Persistence/Photo.cs
+++ TDSPK.API/Infrastructure/Persistence/Photo.cs
@@ -7,23 +7,38 @@ namespace TDSPK.API.Infrastructure.Persistence
     public class Photo
     {
         public Guid Id { get; private set; }
-
-        //public User User { get; private set; }
-
-        [Url]
         public string Url { get; private set; }
-
         public DateTime Date { get; private set; }
 
-        public Photo(string url)
+        // 1..1 - uma foto pertence a um usuário
+        public Guid UserId { get; private set; }
+        public User User { get; set; }
+
+        public Photo(string url, Guid userId)
         {
             if (url == string.Empty) throw new Exception("Url vazia");
 
             Id = Guid.NewGuid();
             Date = DateTime.Now;
             Url = url;
+            UserId = userId;
+        }
 
-            //User = user;
-        }      
+        public void ValidateUrl(string url)
+        {
+            if (string.IsNullOrWhiteSpace(url)) {
+                
+                // Manda o erro para a aplicação de log
+                throw new Exception("URL não pode ser nula ou vazia.");
+            }
+
+            if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+                throw new Exception("URL inválida.");
+        }
+
+        internal static Photo Create(string url, Guid id)
+        {
+            return new Photo(url, id);
+        }
     }
 }
--- TDSPK.API/Infrastructure/Persistence/User.cs
+++ TDSPK.API/Infrastructure/Persistence/User.cs
@@ -6,11 +6,23 @@ namespace TDSPK.API.Infrastructure.Persistence
     public class User : Audit
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }      
+        public string Name { get; set; }
 
-        public User()
+        // 1..N - um usuário tem 1 ou mais fotos
+        public readonly List<Photo> _photos = new();
+
+        public IReadOnlyCollection<Photo> Photos => _photos.AsReadOnly(); // a leitura é acessada mas apenas eu consigo alterar essa lista de fotos
+
+        public User(string name)
         {
+            Id = Guid.NewGuid();
+            Name = name ?? throw new Exception("Nome não pode ser vazio"); // verifica se está nulo
             Status = StatusType.Active;
+        }
+
+        public void AddPhoto(string url) {
+            var photo = Photo.Create(url, Id);
+            _photos.Add(photo);
         }
     }
 }
